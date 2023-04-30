ad58319ff8944f47b46b5e2420426e67002d0e10
Nam3001 <nguyenvn30012003@gmail.com>
2023-04-30 09:37:45 +0700

    update baseURL of api

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: 'https://fake-api-trello.herokuapp.com/api',
+    baseURL: 'https://github.com/Nam3001/trello/api',
     headers: {
         'Content-Type': 'application/json',
     },
